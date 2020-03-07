143d87a130564678392ddf946203715d71d8233b
gzimm4 <60196086+gzimm4@users.noreply.github.com>
2020-03-07 01:06:25 -0700

    Update silvRScript.js

--- Resources/js/silvRScript.js
+++ Resources/js/silvRScript.js
@@ -6,6 +6,6 @@ function send_GA_Event(action_name,action_category,action_label,value) {
 
 	action_category=action_category || 'General';
 
-	gtag(‘event’,action_name, 
-		{‘event_category’ : action_category ,‘event_label’ : action_label,‘value’ : value});
+	gtag('event',action_name, 
+		{'event_category' : action_category ,'event_label' : action_label,'value' : value});
 }
