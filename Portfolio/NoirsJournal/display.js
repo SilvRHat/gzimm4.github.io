73b305e23901c0006dd0c8f1e2fbd87e09d6aa3c
gzimm4 <60196086+gzimm4@users.noreply.github.com>
2021-04-28 15:39:01 -0600

    art project initial commit

---
+++ Portfolio/NoirsJournal/display.js
@@ -0,0 +1,21 @@
+
+const urlParams = new URLSearchParams(window.location.search);
+var lens_str = urlParams.get('lens') || 'norm';
+var lens = lens_str.split(',');
+console.log(lens)
+
+var i, j;
+
+
+for (i=0; i<lens.length; i++) {
+	var pieces = document.getElementsByClassName(lens[i]);
+	console.log(pieces, pieces.len)
+	for (j=0; j<pieces.length; j++) {
+		pieces.item(j).style.visibility = 'visible';
+	};
+	
+	if (lens=='ghost') {
+		document.body.style['background-blend-mode']='luminosity';
+		document.body.style['background-color']='rgb(74 116 232 / 45%)';
+	};
+};
