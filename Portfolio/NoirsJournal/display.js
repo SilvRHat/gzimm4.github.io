a84bbdde1027b6eb7667d460c6425df0a4f9510d
Gavin <60196086+SilvRHat@users.noreply.github.com>
2021-07-19 04:42:33 -0600

    updated look and content

--- Portfolio/NoirsJournal/display.js
+++ Portfolio/NoirsJournal/display.js
@@ -1,4 +1,7 @@
+// Art Project 4 - Noir's Journal
+// Gavin Zimmerman
 
+// Lens Handler - Enables visibility for the components of webpage given the lens in url
 const urlParams = new URLSearchParams(window.location.search);
 var lens_str = urlParams.get('lens') || 'norm';
 var lens = lens_str.split(',');
